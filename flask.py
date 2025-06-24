305bdc19dfa5a4d3e2bc5be059ef0bbadf83da03
Danniell Sanchez <sanchezdanniell@gmail.com>
2025-06-23 21:23:07 -0400

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,1 +1,8 @@
 from flask import Flask, render_template, request, redirect, flash, url_for
+
+
+app = Flask(__name__) #Creates instance of Flask and assigns to variable app. __name__ establishes the current directory that the flask app lives in, to find /static and /templates
+
+users = [] #empty user llst for our instance testing, before we move on to databases for permanent save.
+
+
