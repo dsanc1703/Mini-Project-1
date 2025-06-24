9ec31e333767b702dc94c56619758aa49ad31301
Danniell Sanchez <sanchezdanniell@gmail.com>
2025-06-23 21:39:35 -0400

    Update flask.py

--- flask.py
+++ flask.py
@@ -6,3 +6,10 @@ app = Flask(__name__) #Creates instance of Flask and assigns to variable app. __
 users = [] #empty user llst for our instance testing, before we move on to databases for permanent save.
 
 
+
+@app.route('/signup', methods=['GET', 'POST']) #/signup lets Flask know that it should handle requests to /signup. methods list the http methods that Flask should handle from /signup
+def signup():    #signup function defines what to do when /signup is accessed
+  if request.method == 'POST':   #if the user has clicked the submit button which POST
+    user = request.form.get('username')
+    pass = request.form.get('password')
+    user.append.('user':user, 'pass':pass)  #adds user and pass to the users list as a dictionary value. So the list would look like ['user':'bob','pass':'bob123']
